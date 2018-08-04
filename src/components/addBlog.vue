a725bb7b466329e898414d506d66c3ff900c955c
Max Ivanov <mak.ivanov@gmail.com>
2018-08-04 17:55:42 +0300

    added an add-blog component

---
+++ src/components/addBlog.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="add-blog">
+    <h2>Add a New Blog Post</h2>
+    <form>
+      <label for="blogTitle">Blog Title:</label>
+      <input
+        type="text"
+        v-model.lazy="blog.title"
+        id="blogTitle"
+        required />
+      <label for="blogContent">Blog Content:</label>
+      <textarea
+        v-model.lazy="blog.content"
+        id="blogContent">
+      </textarea>
+    </form>
+    <div id="preview">
+      <h3>Preview Blog</h3>
+      <p>Blog title: {{blog.title}}</p>
+      <p>Blog content: </p>
+      <p>{{blog.content}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      blog: {
+        title: '',
+        content: ''
+      }
+    }
+  },
+  methods: {
+    //
+  }
+}
+</script>
+
+<style>
+#add-blog *{
+    box-sizing: border-box;
+}
+#add-blog{
+    margin: 20px auto;
+    max-width: 500px;
+}
+label{
+    display: block;
+    margin: 20px 0 10px;
+}
+input[type="text"], textarea{
+    display: block;
+    width: 100%;
+    padding: 8px;
+}
+#preview{
+    padding: 10px 20px;
+    border: 1px dotted #ccc;
+    margin: 30px 0;
+}
+h3{
+    margin-top: 10px;
+}
+</style>
